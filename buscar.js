bec52beb5ea570b4c3f915d5f7eff4b499bd3efa
Alma R. Bolaños Villegas <arbolanos@grupomavi.com>
2019-01-24 10:56:59 -0600

    ExtraccionTablas

--- buscar.js
+++ buscar.js
@@ -2,30 +2,27 @@ const fs = require('fs')
 const path = require('path')
 
 var ruta = '../../../Documents/MIGRACION/BusquedadTablas/test'
-var nuevaCarpeta = 'test1/'
+var Archivo = 'test1/Tablas.txt'
 var files = fs.readdirSync(ruta).filter(x => ['.vis','.rep'].indexOf(path.extname(x)) > -1)
 
+let listatablas=['CFDFlexEmpresaGral','Empresa']
 files.forEach(file => {
- fileType = fs.statSync(path.join(ruta,file))
- if(fileType.isFile()){
-  var texto = fs.readFileSync(path.join(ruta,file),{encoding:'latin1'}) 
- }
-     if(/\[Vista[\W\w]*?(?=(^\[|^$))/m.test(texto))
-        {
-            // fs.writeFileSync('test/texto.txt', texto)
-            let vista = texto.match(/\[Vista[\W\w]*?(?=^\[)/m).join('')
-            
-            fs.writeFileSync('test/vista.txt', vista)
-        }
-      //texto = texto.match(/^\[Vista[\W\w]*?(?=(^\[|^$))/m)
-      //texto = texto.match(/ListaTablas=\w*/gim)
-      //texto = texto.match(/VistaIndependiente=\w*/gim)
-      console.log(texto)
-      //console.log(texto.match(/ListaTablas=\w*/gim))
-      //console.log(texto.match(/VistaIndependiente=\w*/gim))
-     fs.writeFile(nuevaCarpeta+file, texto, function (err) {
-        if (err) {
-          return console.log(err)
+    fileType = fs.statSync(path.join(ruta,file))
+    if(fileType.isFile()){
+        var texto = fs.readFileSync(path.join(ruta,file),{encoding:'latin1'}) 
+    }
+    if(/\[Vista[\W\w]*?(?=(^\[|^$))/m.test(texto)){
+        let vista = texto.match(/\[Vista[\W\w]*?(?=^\[)/m).join('')
+        if(/VistaIndependiente=\w*/gim.test(vista)){}
+        else if (/ListaTablas=\w.*/gim.test(vista)){
+            let vista1 = texto.match(/(?<=ListaTablas=).*/gim).join('')
+            let separar= vista1.split('<BR>')
+            separar.forEach(x => {
+                listatablas.push(x)
+            })
+            fs.writeFileSync(Archivo, listatablas)
+            }
         }
     })
-})
+console.log(listatablas)
+
